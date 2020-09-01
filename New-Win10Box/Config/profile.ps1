fc82dd9efc5d93245d031dfc9807ee9455e388d3
Jerome <46498335+VV-B0Y@users.noreply.github.com>
2020-09-01 03:20:31 -0400

    Added settings files $profile and settings.json

---
+++ New-Win10Box/Config/profile.ps1
@@ -0,0 +1,95 @@
+﻿Clear-Host
+
+# Get random cat fact
+function Kitty {
+	Write-Host (Invoke-RestMethod catfact.ninja/facts).data.fact -BackgroundColor green -ForegroundColor black
+}
+Kitty
+
+trap { Write-Warning ($_.ScriptStackTrace | Out-String) }
+
+# This timer is used by Trace-Message, I want to start it immediately
+$TraceVerboseTimer = New-Object System.Diagnostics.Stopwatch
+$TraceVerboseTimer.Start()
+
+# Note these are dependencies of the Profile module, but it's faster to load them explicitly up front
+Import-Module -FullyQualifiedName @{ ModuleName = "Environment"; ModuleVersion = "1.1.0" },
+@{ ModuleName = "Configuration"; ModuleVersion = "1.3.1" },
+@{ ModuleName = "Pansies"; ModuleVersion = "1.2.1" },
+@{ ModuleName = "PowerLine"; ModuleVersion = "3.1.2" },
+@{ ModuleName = "PSReadLine"; ModuleVersion = "2.0.0" },
+@{ ModuleName = "DefaultParameter"; ModuleVersion = "1.7" },
+@{ ModuleName = "Terminal-Icons"; ModuleVersion = "0.1.1" } #-Verbose:$false
+
+# First call to Trace-Message, pass in our TraceTimer that I created at the top to make sure we time EVERYTHING.
+# This has to happen after the verbose check, obviously
+Trace-Message "Modules Imported" -Stopwatch $TraceVerboseTimer
+Trace-Message "Profile Finished!" -KillTimer
+Remove-Variable TraceVerboseTimer
+
+## Relax the code signing restriction so we can actually get work done
+Set-ExecutionPolicy RemoteSigned Process
+$VerbosePreference = "SilentlyContinue"
+
+#New-PromptText  "I $( "&hearts;" -fg "DarkRed") PWSH'n it> "
+#Set-PowerLinePrompt -PowerLineFont -FullColor -Newline -Timestamp -Colors "#fe4450", "#ff7edb"
+Set-Location '.\'
+
+# function prompt {
+# "I $(New-Text "&hearts;" -fg "DarkRed") PWSH'n it> "
+# Set-PowerLinePrompt -SetCurrentDirectory -Newline -Timestamp -Colors "#fe4450", "#ff7edb"
+# $Host.UI.RawUI.WindowTitle = ( Get-Date -UFormat '%m/%d/%y %R').Tostring()
+# }
+# [System.Collections.Generic.List[ScriptBlock]]$Prompt = @(
+# { "PS " }
+# { $executionContext.SessionState.Path.CurrentLocation }
+# { '>' * ($nestedPromptLevel + 1) }
+# { $Host.UI.RawUI.WindowTitle = ( Get-Date -UFormat '%m/%d/%y %R').Tostring() }
+# )
+# if (Test-Success) {
+# $heart = "❤", "🧡", "💛", "💚", "💙", "💜", "💔", "💕", "💓", "💗", "💖", "💘", "💝" | Get-Random
+# }
+# New-PromptText "I${heart}PS" -ForegroundColor Black -BackgroundColor White -ErrorForegroundColor White -ErrorBackgroundColor DarkRed #>
+# Set-PowerLinePrompt -SetCurrentDirectory -FullColor -Newline -Timestamp -Colors "#fe4450", "#ff7edb"
+
+#Stop Teams status from showing idle
+Function Move-Mouse {
+	$WShell = New-Object -com "Wscript.Shell"
+	while ($true) {
+		$WShell.sendkeys("{SCROLLLOCK}")
+		Start-Sleep -Milliseconds 100
+		$WShell.sendkeys("{SCROLLLOCK}")
+		Start-Sleep -Seconds 240
+	}
+}
+
+# Connect to teams with credentials choosen from Out-GridView
+function Connect-Teams {
+	$creds = Get-ChildItem 'C:\Temp\cred' | Out-GridView -PassThru
+	$UserCredential = Import-Clixml -Path "C:\Temp\cred\$creds"
+	Connect-MicrosoftTeams -Credential $UserCredential
+}
+
+# Get Weather
+function Show-Weather {
+	param (
+		$city
+	)
+	if (-not $city) {
+		(Invoke-RestMethod http://wttr.in/philadelphia)
+	}
+	else {
+		(Invoke-RestMethod http://wttr.in/$city)
+	}
+}
+
+# Get console history
+function Chistory {
+	(notepad++ C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt)
+}
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+	Import-Module "$ChocolateyProfile"
+}
---
+++ New-Win10Box/FreshBox.ps1
@@ -0,0 +1,103 @@
+﻿#Requires -RunAsAdministrator
+
+[cmdletbinding()]
+param (
+	[switch]$UACNoConsent
+)
+
+# Execution policies
+Set-ExecutionPolicy RemoteSigned -Scope Process
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine
+
+# Disable BitLocker
+manage-bde -off C:
+
+if ($UACNoConsent) {
+	# Regkey to turn off UAC consent prompt behavior for Admins; NOT disabling UAC
+	Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'ConsentPromptBehaviorAdmin' -Value 0
+}
+
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Setup PSGallery
+Install-PackageProvider -Name Nuget -Scope CurrentUser -Force -Confirm:$false
+
+# Install modules with required version (Windows Powershell Profile Dependencies)
+$RvModList = @{
+	'Configuration'    = '1.3.1'
+	'Environment'      = '1.1.0'
+	'DefaultParameter' = '1.7'
+	'Pansies'          = '1.2.1'
+	'Powerline'        = '3.1.2'
+	'Terminal-Icons'   = '0.1.1'
+}
+ForEach ($RvModule in $RvModList.Keys) {
+	Install-Module $RvModule -RequiredVersion $RvModList[$RvModule] -Force -Confirm:$false
+}
+
+# Install modules (No req version)
+$ModList = @(
+	'AzureAD'
+	'MicrosoftTeams'
+	'PsKoans'
+	'Posh365'
+)
+ForEach ($Module in $ModList) {
+	Install-Module $Module -Force -Confirm:$false
+}
+
+
+$PackageList = @(
+	'discord'
+	'everything'
+	'FireFox'
+	'git'
+	'GoogleChrome'
+	'microsoft-windows-terminal'
+	'microsoft-teams'
+	'notepadplusplus.install'
+	'powertoys'
+	'sharex'
+	'vmware-workstation-player'
+	'vmware-tools'
+	'VsCode'
+	'vscode-csharpextensions'
+	'vscode-powershell'
+)
+
+choco install $PackageList -fy
+
+# Alias "pog" stands for "pretty log"
+git config --global alias.pog "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# Make a place for Git repos to live
+New-Item -Path c:\git -ItemType Directory -Force -ErrorAction 0
+New-Item -Path c:\temp -ItemType Directory -Force -ErrorAction 0
+
+# Show extensions for known file types; current user
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
+
+# Show extensions for known file types; all users
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\HideFileExt" -Name "DefaultValue" -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced\Folder\HideFileExt" -Name "CheckedValue" -Value 0
+
+# Install WSL & make v2 default
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+do {
+	$WslVersion = wsl --set-default-version 2
+	if (($WslVersion).split(':')[0] -eq 'Error') {
+		Invoke-RestMethod https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile c:/temp/wsl2.msi
+		\.\temp\wsl2.msi /quiet
+	}
+} until (($WslVersion).split(':')[0] -ne 'Error')
+
+# Import My Windows Posh Profile
+$WpProfile = (Invoke-WebRequest 'https://gist.githubusercontent.com/VV-B0Y/962f6baed1f09b4eedc06ceb2baa0535/raw/bf16bcacb38bb9083ce6afb337c6263837b880e6/Microsoft.PowerShell_profile.ps1' -UseBasicParsing).Content
+Set-Content -Value $WpProfile -Path "$env:USERPROFILE/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1"
+
+# Import My Windows Terminal Settings
+$WtSettingPath = (Get-ChildItem -Directory $env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_*\LocalState\).fullname
+$wtProfileContent = (Invoke-WebRequest 'https://gist.githubusercontent.com/VV-B0Y/c327d1c9a585b200ca3fe159999ea1f9/raw/f8c2080fc3aa77a448f159988c6c498f6881c20b/settings.json' -UseBasicParsing).Content
+Set-Content -Value $wtProfileContent -Path ('{0}\{1}' -f $WtSettingPath, 'settings.json')
